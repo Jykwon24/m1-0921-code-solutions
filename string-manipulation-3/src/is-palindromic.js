442c917e38804d88f23ada911054078447515afe
Jeff Kwon <jykwon007@gmail.com>
2022-01-19 17:59:30 -0800

    Completed string-manipulation-3 exercsise.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,19 @@
+/* exported isPalindromic */
+function isPalindromic(string) {
+  var reg = /[\W]/g;
+
+  var editString = string.replace(reg, '');
+  var reverse = editString.split('').reverse().join('');
+
+  if (reverse === editString) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+// #1
+// Declare function isPalindromic, which takes one argument which is a string
+// Turn string into an array and then reverse the order
+// Turn the reversed array back into string and check if it matches the original
